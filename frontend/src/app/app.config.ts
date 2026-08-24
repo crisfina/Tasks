70859c9ebb01ede232ffad205e3f0807ea6b52b1
crisfina <cristinap.azu@gmail.com>
2026-08-24 16:12:20 +0200

    Configure Angular API client

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,7 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners,} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { API_BASE_URL } from './core/api/api.config';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
+    {
+      provide: API_BASE_URL,
+      useValue: 'http://127.0.0.1:8000',
+    },
+    provideRouter(routes),
+  ],
 };
