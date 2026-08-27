9bd130e4837cde8d7467044301462ea511e23223
crisfina <cristinap.azu@gmail.com>
2026-08-27 11:39:15 +0200

    CORS allow angular from github

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,6 +32,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:4200",
         "http://127.0.0.1:4200",
+        "https://crisfina.github.io",
     ],
     allow_credentials=True,
     allow_methods=["*"],
